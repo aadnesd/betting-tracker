230a9cbf56ac832a01348109276fe6c2f03d6f84
Ådne S Djuve <aadne.s.djuve@gmail.com>
2026-03-07 19:36:03 +0000

    Ignore non-pooling URL on Vercel builds

--- lib/db/get-connection-string.ts
+++ lib/db/get-connection-string.ts
@@ -3,6 +3,10 @@ export function getPostgresUrl() {
 }
 
 export function getMigrationPostgresUrl() {
+  if (process.env.VERCEL) {
+    return process.env.POSTGRES_URL ?? process.env.APP_POSTGRES_URL;
+  }
+
   return (
     process.env.POSTGRES_URL_NON_POOLING ??
     process.env.POSTGRES_URL ??
@@ -11,6 +15,18 @@ export function getMigrationPostgresUrl() {
 }
 
 export function getMigrationPostgresSource() {
+  if (process.env.VERCEL) {
+    if (process.env.POSTGRES_URL) {
+      return "POSTGRES_URL";
+    }
+
+    if (process.env.APP_POSTGRES_URL) {
+      return "APP_POSTGRES_URL";
+    }
+
+    return null;
+  }
+
   if (process.env.POSTGRES_URL_NON_POOLING) {
     return "POSTGRES_URL_NON_POOLING";
   }
